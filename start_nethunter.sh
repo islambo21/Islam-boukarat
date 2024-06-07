e4af8b63c00758d9895dc18245f56f6b907462d6
islambo21 <160646633+islambo21@users.noreply.github.com>
2024-06-07 23:50:16 +0100

    Update start_nethunter.sh

--- start_nethunter.sh
+++ start_nethunter.sh
@@ -14,7 +14,7 @@ echo -e "\e[96mWelcome! Please choose an option by entering the corresponding nu
 read -p "Enter your username: " username
 
 # طباعة الخيارات
-echo "Enter your choice: "
+echo -e "\e[94mEnter your choice:\e[0m"
 echo -e "\e[32m[1] Facebook\e[0m"
 echo -e "\e[33m[2] Instagram\e[0m"
 echo -e "\e[34m[3] Snapchat\e[0m"
@@ -26,6 +26,9 @@ echo
 # قراءة الاختيار من المستخدم
 read choice
 
+# تعيين تأخير بسيط
+delay=2
+
 case $choice in
     1)
         # تنظيف الشاشة
@@ -33,32 +36,44 @@ case $choice in
         # عرض النص الكبير باستخدام figlet مع الألوان
         figlet -f slant "Facebook" | lolcat -p 1.0
         # رسالة التشجيع للانتقال إلى الواجهة التالية
-        echo -e "\e[96mWelcome to Facebook hacking interface, $username!\e[0m"
+        echo -e "\e[96mSearching for Facebook password of user: $username...\e[0m"
+        sleep $delay
+        echo -e "\e[96mFacebook password found: $(openssl rand -base64 12)\e[0m"
         ;;
     2)
         clear
         figlet -f slant "Instagram" | lolcat -p 1.0
-        echo -e "\e[96mWelcome to Instagram hacking interface, $username!\e[0m"
+        echo -e "\e[96mSearching for Instagram password of user: $username...\e[0m"
+        sleep $delay
+        echo -e "\e[96mInstagram password found: $(openssl rand -base64 12)\e[0m"
         ;;
     3)
         clear
         figlet -f slant "Snapchat" | lolcat -p 1.0
-        echo -e "\e[96mWelcome to Snapchat hacking interface, $username!\e[0m"
+        echo -e "\e[96mSearching for Snapchat password of user: $username...\e[0m"
+        sleep $delay
+        echo -e "\e[96mSnapchat password found: $(openssl rand -base64 12)\e[0m"
         ;;
     4)
         clear
         figlet -f slant "Gmail" | lolcat -p 1.0
-        echo -e "\e[96mWelcome to Gmail hacking interface, $username!\e[0m"
+        echo -e "\e[96mSearching for Gmail password of user: $username...\e[0m"
+        sleep $delay
+        echo -e "\e[96mGmail password found: $(openssl rand -base64 12)\e[0m"
         ;;
     5)
         clear
         figlet -f slant "Telegram" | lolcat -p 1.0
-        echo -e "\e[96mWelcome to Telegram hacking interface, $username!\e[0m"
+        echo -e "\e[96mSearching for Telegram password of user: $username...\e[0m"
+        sleep $delay
+        echo -e "\e[96mTelegram password found: $(openssl rand -base64 12)\e[0m"
         ;;
     6)
         clear
         figlet -f slant "Free Fair" | lolcat -p 1.0
-        echo -e "\e[96mWelcome to Free Fair hacking interface, $username!\e[0m"
+        echo -e "\e[96mSearching for Free Fair password of user: $username...\e[0m"
+        sleep $delay
+        echo -e "\e[96mFree Fair password found: $(openssl rand -base64 12)\e[0m"
         ;;
     *)
         echo "Invalid choice. Please try again."
