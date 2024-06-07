665b369db01539d974216007cabd61ee0029bd10
Islam boukarat <Iboukrat@gmail.com>
2024-06-07 18:55:05 +0100

    Add start_nethunter script

---
+++ start_nethunter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+
+# عرض النص الكبير باستخدام figlet
+figlet -f slant "ISLAM"
+echo "NetHunter NetHunter"
+
+echo "[+] NetHunter for Termux installed successfully"
+echo
+echo "[+] To start NetHunter, type:"
+echo "[1] nethunter                # To start NetHunter cli"
+echo "[2] nethunter kex passwd     # To set the KeX password"
+echo "[3] nethunter kex &          # To start NetHunter gui"
+echo "[4] nethunter kex stop       # To stop NetHunter gui"
+echo "[5] nethunter -r             # To run NetHunter as root"
+echo "[6] nh                      # Shortcut for nethunter"
+echo
+
+# أوامر إضافية حسب الحاجة
