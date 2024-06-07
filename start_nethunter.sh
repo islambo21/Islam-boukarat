26b8e2fdd296f317079e03664f9694c9f823a916
islambo21 <160646633+islambo21@users.noreply.github.com>
2024-06-07 23:29:51 +0100

    Update start_nethunter.sh

--- start_nethunter.sh
+++ start_nethunter.sh
@@ -2,15 +2,15 @@
 
 clear
 
-# عرض النص الكبير باستخدام toilet بزخرفة جميلة
-toilet -f smblock "ISLAM" --filter border --gay | lolcat -p 1.0
+# عرض النص الكبير باستخدام figlet
+figlet -f slant "ISLAM" | lolcat -p 1.0
 echo "NetHunter NetHunter"
 
 # طلب اسم المستخدم من المستخدم
-read -p "$(tput setaf 3)Enter your username: $(tput sgr0)" username
+read -p "Enter your username: " username
 
-# طباعة الخيارات بألوان مختلفة وتنسيق
-echo "$(tput setaf 6)Enter your choice:$(tput sgr0)"
+# طباعة الخيارات
+echo "Enter your choice: "
 echo -e "\e[32m[1] Facebook\e[0m"
 echo -e "\e[33m[2] Instagram\e[0m"
 echo -e "\e[34m[3] Snapchat\e[0m"
@@ -24,38 +24,38 @@ read choice
 case $choice in
     1)
         clear
-        # عرض رسالة كأنك تقوم بالبحث والاختراق كهاكر باستخدام toilet
-        toilet -f term -F border "Searching for Facebook password of user: $username" --gay | lolcat -p 1.0
+        # عرض رسالة كأنك تقوم بالبحث والاختراق كهاكر
+        echo "Searching for Facebook password of user: $username"
         echo "Facebook password found: $(openssl rand -base64 12)"
         ;;
     2)
         clear
-        # عرض رسالة كأنك تقوم بالبحث والاختراق كهاكر باستخدام toilet
-        toilet -f term -F border "Searching for Instagram password of user: $username" --gay | lolcat -p 1.0
+        # عرض رسالة كأنك تقوم بالبحث والاختراق كهاكر
+        echo "Searching for Instagram password of user: $username"
         echo "Instagram password found: $(openssl rand -base64 12)"
         ;;
     3)
         clear
-        # عرض رسالة كأنك تقوم بالبحث والاختراق كهاكر باستخدام toilet
-        toilet -f term -F border "Searching for Snapchat password of user: $username" --gay | lolcat -p 1.0
+        # عرض رسالة كأنك تقوم بالبحث والاختراق كهاكر
+        echo "Searching for Snapchat password of user: $username"
         echo "Snapchat password found: $(openssl rand -base64 12)"
         ;;
     4)
         clear
-        # عرض رسالة كأنك تقوم بالبحث والاختراق كهاكر باستخدام toilet
-        toilet -f term -F border "Searching for Gmail password of user: $username" --gay | lolcat -p 1.0
+        # عرض رسالة كأنك تقوم بالبحث والاختراق كهاكر
+        echo "Searching for Gmail password of user: $username"
         echo "Gmail password found: $(openssl rand -base64 12)"
         ;;
     5)
         clear
-        # عرض رسالة كأنك تقوم بالبحث والاختراق كهاكر باستخدام toilet
-        toilet -f term -F border "Searching for Telegram password of user: $username" --gay | lolcat -p 1.0
+        # عرض رسالة كأنك تقوم بالبحث والاختراق كهاكر
+        echo "Searching for Telegram password of user: $username"
         echo "Telegram password found: $(openssl rand -base64 12)"
         ;;
     6)
         clear
-        # عرض رسالة كأنك تقوم بالبحث والاختراق كهاكر باستخدام toilet
-        toilet -f term -F border "Searching for Free Fair password of user: $username" --gay | lolcat -p 1.0
+        # عرض رسالة كأنك تقوم بالبحث والاختراق كهاكر
+        echo "Searching for Free Fair password of user: $username"
         echo "Free Fair password found: $(openssl rand -base64 12)"
         ;;
     *)
