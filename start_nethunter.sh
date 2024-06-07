c0b847b1edf4192bfe0b46bd3c651dc4215ce613
Islam boukarat <Iboukrat@gmail.com>
2024-06-07 21:36:00 +0100

    وصف التغييرات

--- start_nethunter.sh
+++ start_nethunter.sh
@@ -8,13 +8,40 @@ echo "NetHunter NetHunter"
 
 echo "[+] NetHunter for Termux installed successfully"
 echo
-echo "[+] To start NetHunter, type:"
-echo "[1] nethunter                # To start NetHunter cli"
-echo "[2] nethunter kex passwd     # To set the KeX password"
-echo "[3] nethunter kex &          # To start NetHunter gui"
-echo "[4] nethunter kex stop       # To stop NetHunter gui"
-echo "[5] nethunter -r             # To run NetHunter as root"
-echo "[6] nh                      # Shortcut for nethunter"
+
+# استخدام ألوان ANSI للخيارات
+echo -e "\e[32m[1] Hack Facebook\e[0m"
+echo -e "\e[33m[2] Hack Instagram\e[0m"
+echo -e "\e[34m[3] Hack Snapchat\e[0m"
+echo -e "\e[35m[4] Hack Gmail\e[0m"
+echo -e "\e[36m[5] Hack Telegram\e[0m"
 echo
 
-# أوامر إضافية حسب الحاجة
+echo "Enter your choice: "
+read choice
+
+case $choice in
+    1)
+        echo "Executing Facebook hack..."
+        # ضع هنا الأمر أو السكريبت الذي ترغب في تشغيله
+        ;;
+    2)
+        echo "Executing Instagram hack..."
+        # ضع هنا الأمر أو السكريبت الذي ترغب في تشغيله
+        ;;
+    3)
+        echo "Executing Snapchat hack..."
+        # ضع هنا الأمر أو السكريبت الذي ترغب في تشغيله
+        ;;
+    4)
+        echo "Executing Gmail hack..."
+        # ضع هنا الأمر أو السكريبت الذي ترغب في تشغيله
+        ;;
+    5)
+        echo "Executing Telegram hack..."
+        # ضع هنا الأمر أو السكريبت الذي ترغب في تشغيله
+        ;;
+    *)
+        echo "Invalid choice. Please try again."
+        ;;
+esac
